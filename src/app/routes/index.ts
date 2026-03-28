4c68d939b1e895512286f9c7440ca0a91c2de9b2
Rafiul29 <rafiulislam4472@gmail.com>
2026-03-28 14:27:13 +0600

    admin routes added

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { AuthRoutes } from "../module/auth/auth.route";
+import { AdminRoutes } from "../module/admin/admin.route";
 const router = Router();
 
 router.use("/auth", AuthRoutes);    
-
+router.use("/admins", AdminRoutes)
 
 
 export const IndexRoutes:Router = router;
